4e5eb71ec315b80f1aed61f4fba6f401f6574090
1Kostik <sorochankostya@gmail.com>
2024-06-10 19:12:26 +0300

    added

--- src/components/CardSlider/CardSlider.tsx
+++ src/components/CardSlider/CardSlider.tsx
@@ -1,6 +1,8 @@
-import { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css/navigation";
 import "swiper/css";
+import "./CardSlaider.css";
 import { containerStyles } from "@styles/variables";
 import {
   ArrowContainer,
@@ -10,6 +12,7 @@ import {
   TitleContainer,
   arrowLeft,
   arrowRight,
+  reviews,
   swiper,
 } from "./CardSlider.styled";
 import foto1 from "@assets/images/imagesSwiper/photo (2).png";
@@ -17,86 +20,163 @@ import foto2 from "@assets/images/imagesSwiper/photo (1).png";
 import foto3 from "@assets/images/imagesSwiper/teleg (1).png";
 import foto4 from "@assets/images/imagesSwiper/teleg (2).png";
 import foto5 from "@assets/images/imagesSwiper/teleg (3).png";
-import { Swiper as SwiperClass } from "swiper/types";
 import { ReactComponent as ArrowRight } from "@assets/icons/arrow-right.svg";
 import { ReactComponent as ArrowLeft } from "@assets/icons/arrow-left.svg";
+import { ReactComponent as ArrowShortLeft } from "@assets/icons/arrow-short-left.svg";
+import { ReactComponent as ArrowShortRight } from "@assets/icons/arrow-short-right.svg";
+import { CardItem } from "@components/CardItem";
+import { Navigation } from "swiper/modules";
+import { useLocation } from "react-router-dom";
+import SwiperCore from "swiper";
 
 const CardSlider = () => {
-  const [swiperRef, setSwiperRef] = useState<SwiperClass | null>(null);
-
-  const updateNavigationState = (swiper: SwiperClass) => {
-    if (swiper.isBeginning) {
-      document.getElementById("prevButton")!.style.visibility = "hidden";
-    } else {
-      document.getElementById("prevButton")!.style.visibility = "visible";
-    }
-    if (swiper.isEnd) {
-      document.getElementById("nextButton")!.style.visibility = "hidden";
-    } else {
-      document.getElementById("nextButton")!.style.visibility = "visible";
-    }
-  };
-
-  const handleClick = (direction: string) => {
-    if (direction === "right") {
-      swiperRef?.slideNext();
-    } else {
-      swiperRef?.slidePrev();
-    }
-  };
-
+  const location = useLocation();
+  const isMainPage = location.pathname === "/";
+  const [swiperRef, setSwiperRef] = useState<SwiperCore | null>(null);
   useEffect(() => {
     if (swiperRef) {
-      updateNavigationState(swiperRef);
-      swiperRef.on("slideChange", () => updateNavigationState(swiperRef));
+      const updateNavigationState = () => {
+        const prevButton = document.getElementById("prevButton");
+        const nextButton = document.getElementById("nextButton");
+
+        if (prevButton && nextButton) {
+          if (swiperRef.isBeginning) {
+            prevButton.style.visibility = "hidden";
+          } else {
+            prevButton.style.visibility = "visible";
+          }
+
+          if (swiperRef.isEnd) {
+            nextButton.style.visibility = "hidden";
+          } else {
+            nextButton.style.visibility = "visible";
+          }
+        }
+      };
+
+      swiperRef.on("slideChange", updateNavigationState);
+      updateNavigationState(); // Initial check
     }
   }, [swiperRef]);
 
+  const handleSwiper = (swiper: SwiperCore) => {
+    setSwiperRef(swiper);
+  };
+
+  const images = [foto1, foto2, foto3, foto4, foto5];
+  const text = [
+    {
+      id: 1,
+      name: "Lera",
+      activity: "фітнес тренер",
+      description:
+        "Знання в школі допомогли мені створити свою лінію аромакомпозицій для клієнтів які домогамають отримати найшвидкіші результати в сфері краси тіла",
+    },
+    {
+      id: 2,
+      name: "Sveta",
+      activity: "домогосподарка, мама",
+      description:
+        "Ароматерапія, навіть з першого курсу, відкрилася для мене як спосіб допомоги рідним природним способом. Я змінила звичні ліки на аромааптечку. Дякую",
+    },
+    {
+      id: 3,
+      name: "Lena",
+      activity: "масажист, психолог",
+      description:
+        "Дякую за знання! Вони допомогли мені мати свій особливий відхід до допомоги клієнтам. Мало хто з колег працює професійно з ефірними оліями.",
+    },
+    {
+      id: 4,
+      name: "Anna",
+      activity: "студент",
+      description:
+        "Знання в школі допомогли мені створити свою лінію аромакомпозицій для клієнтів які домогамають отримати найшвидкіші результати в сфері краси тіла",
+    },
+    {
+      id: 5,
+      name: "Anna",
+      activity: "студент",
+      description:
+        "Знання в школі допомогли мені створити свою лінію аромакомпозицій для клієнтів які домогамають отримати найшвидкіші результати в сфері краси тіла",
+    },
+    {
+      id: 6,
+      name: "Anna",
+      activity: "студент",
+      description:
+        "Знання в школі допомогли мені створити свою лінію аромакомпозицій для клієнтів які домогамають отримати найшвидкіші результати в сфері краси тіла",
+    },
+    {
+      id: 7,
+      name: "Anna",
+      activity: "студент",
+      description:
+        "Знання в школі допомогли мені створити свою лінію аромакомпозицій для клієнтів які домогамають отримати найшвидкіші результати в сфері краси тіла",
+    },
+  ];
+
   return (
     <section css={containerStyles}>
-      <Container>
+      <Container isMainPage={isMainPage}>
         <TitleContainer>
-          <H2>Діяльність у фотографіях</H2>
-          <ArrowContainer>
-            <Button id="prevButton" onClick={() => handleClick("left")}>
-              <ArrowLeft css={arrowLeft} />
+          {isMainPage ? <H2>Діяльність у фотографіях</H2> : <H2>Відгуки</H2>}
+
+          <ArrowContainer isMainPage={isMainPage}>
+            <Button id="prevButton">
+              {isMainPage ? (
+                <ArrowLeft css={arrowLeft} />
+              ) : (
+                <ArrowShortLeft css={arrowLeft} />
+              )}
             </Button>
-            <Button id="nextButton" onClick={() => handleClick("right")}>
-              <ArrowRight css={arrowRight} />
+            <Button id="nextButton">
+              {isMainPage ? (
+                <ArrowRight css={arrowRight} />
+              ) : (
+                <ArrowShortRight css={arrowLeft} />
+              )}
             </Button>
           </ArrowContainer>
         </TitleContainer>
         <Swiper
-          spaceBetween={12}
-          onSwiper={setSwiperRef}
-          css={swiper}
+          onSwiper={handleSwiper}
+          css={isMainPage ? swiper : reviews}
           breakpoints={{
             360: {
               slidesPerView: 1.06,
+              spaceBetween: 16,
             },
             768: {
-              slidesPerView: 1.5,
+              slidesPerView: isMainPage ? 1.5:2.1 ,
+              spaceBetween: isMainPage ? 12 : 16,
             },
             1440: {
-              slidesPerView: 2.1,
+              slidesPerView: isMainPage ? 2.1 : 3.5,
+              spaceBetween: isMainPage ? 12 : 20,
             },
           }}
+          modules={[Navigation]}
+          navigation={{
+            prevEl: "#prevButton",
+            nextEl: "#nextButton",
+          }}
         >
-          <SwiperSlide>
-            <img src={foto1} alt="Фото 1" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={foto2} alt="Фото 2" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={foto3} alt="Фото 3" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={foto4} alt="Фото 4" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={foto5} alt="Фото 5" />
-          </SwiperSlide>
+          {isMainPage
+            ? images &&
+              images.length > 0 &&
+              images.map((item, i) => (
+                <SwiperSlide className="swiper-slide image-slide" key={i}>
+                  <img src={item} alt="" />
+                </SwiperSlide>
+              ))
+            : text &&
+              text.length > 0 &&
+              text.map((item) => (
+                <SwiperSlide className="swiper-slide text-slide" key={item.id}>
+                  <CardItem text={item} />
+                </SwiperSlide>
+              ))}
         </Swiper>
       </Container>
     </section>
--- src/pages/StorePage/StorePage.tsx
+++ src/pages/StorePage/StorePage.tsx
@@ -1,10 +1,151 @@
 import HeroSection from "@components/HeroSection/HeroSection";
 
-
 function StorePage() {
+  const arrNumders = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   return (
     <>
       <HeroSection viewType={"other"}>Магазин</HeroSection>
+      <section
+        style={{
+          padding: "60px 78px",
+          background: "#252525",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            gap: "24px",
+            width:"1284px",
+            marginLeft: "auto",
+            marginRight: "auto",
+          }}
+        >
+          <div
+            style={{
+              width: "312px",
+              height: "758px",
+              border: "1px solid #d7d7d7",
+              borderRadius: "16px",
+              padding: "20px 26px",
+            }}
+          >
+            <h2 style={{ color: "#d7d7d7" }}>Фільтр</h2>
+          </div>
+          <div
+            style={{
+              width: "948px",
+              height: "1708px",
+            }}
+          >
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                marginBottom: "24px",
+              }}
+            >
+              <input
+                type="text"
+                id="search"
+                placeholder="search"
+                style={{
+                  width: "200px",
+                  height: "40px",
+                  border: "1px solid #d7d7d7",
+                  borderRadius: "24px",
+                  padding: "8px 12px",
+                }}
+              />
+              <div style={{ display: "flex", gap: "24px" }}>
+                <button
+                  style={{
+                    border: "1px solid #d7d7d7",
+                    borderRadius: "24px",
+                    padding: "8px 12px",
+                  }}
+                >
+                  Фільтр
+                </button>
+                <div
+                  style={{ display: "flex", gap: "12px", alignItems: "center" }}
+                >
+                  <h4 style={{ color: "#d7d7d7" }}>Сортувати за</h4>
+                  <select
+                    name=""
+                    id=""
+                    style={{
+                      width: "311px",
+                      height: "40px",
+                      border: "1px solid #d7d7d7",
+                      borderRadius: "24px",
+                      padding: "8px 12px",
+                    }}
+                  ></select>
+                </div>
+              </div>
+            </div>
+
+            <div
+              style={{
+                display: "flex",
+                flexWrap: "wrap",
+                gap: "20px",
+                width: "100%",
+              }}
+            >
+              {arrNumders &&
+                arrNumders.map((item) => (
+                  <div
+                    style={{
+                      width: "302px",
+                      height: "396px",
+                      borderRadius: "16px",
+                    }}
+                    key={item}
+                  >
+                    <div
+                      style={{
+                        width: "302px",
+                        height: "336px",
+                        marginBottom: "8px",
+                        background: "#b7b7b7",
+                        borderRadius: "16px",
+                      }}
+                    >
+                      {/* <img src="" alt="" style={{ border:"none" }} /> */}
+                    </div>
+                    <div
+                      style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        gap: "4px",
+                      }}
+                    >
+                      <h5
+                        style={{
+                          width: "100%",
+                          height: "24px",
+                          color: "#d7d7d7",
+                        }}
+                      >
+                        Назва продукту
+                      </h5>
+                      <p
+                        style={{
+                          width: "100%",
+                          height: "24px",
+                          color: "#d7d7d7",
+                        }}
+                      >
+                        324 грн
+                      </p>
+                    </div>
+                  </div>
+                ))}
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
