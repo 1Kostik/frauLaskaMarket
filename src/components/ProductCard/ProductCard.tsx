981ac874849ca5c3655f2d181b198aab1941dcd5
dimside <dimside29@gmail.com>
2024-07-30 17:58:49 +0300

    add-empty-plug-on-image

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -9,12 +9,15 @@ import {
 } from "./ProductCard.styled";
 import { Product } from "Interfaces/Product";
 import ProductInterface from "@components/ProductInterface";
+import emptyPlug from "@assets/images/emptyPlug.webp"
+
 interface Props {
   show?: boolean;
   handleOnClickCard?: (id: number) => void;
   item: Product;
   width?: string;
 }
+
 const ProductCard: React.FC<Props> = ({
   show,
   handleOnClickCard,
@@ -22,6 +25,11 @@ const ProductCard: React.FC<Props> = ({
   width,
 }) => {
   const { id, imageUrls, title, variations } = item;
+
+  const onSrcImageError = (e: React.SyntheticEvent<HTMLImageElement>) => {
+    const target = e.target as HTMLImageElement;
+    target.src = emptyPlug;
+  };
 
   return (
     <Container
@@ -36,12 +44,15 @@ const ProductCard: React.FC<Props> = ({
             src={imageUrls[0].img_url}
             alt=""
             style={{ width: `${width}` }}
+            onError={onSrcImageError}
           />
           <ProductInterface productId={id} />
         </ImageContainer>
         <InfoContainer>
           <H5>{title}</H5>
-          <P>{variations && variations.length > 0 ? variations[0].price:''} грн</P>
+          <P>
+            {variations && variations.length > 0 ? variations[0].price : ""} грн
+          </P>
         </InfoContainer>
       </CardWrapper>
     </Container>
