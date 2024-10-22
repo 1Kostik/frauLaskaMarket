5fa5a2f3ee701c4cc2bfa4aa40def54f74c5d17c
1Kostik <sorochankostya@gmail.com>
2024-10-22 15:38:22 +0300

    up

--- src/errorHandler/errorHandler.ts
+++ src/errorHandler/errorHandler.ts
@@ -5,16 +5,17 @@ export const handlerAxiosError = (error: unknown) => {
   if (axios.isAxiosError(error)) {
     if (error.response) {
       const status = error.response.status;
-      if (status === 500) {
-        toast.error("Сталася внутрішня помилка на сервері. Спробуйте пізніше.");
-      } else if (status === 503) {
-        toast.error("Сервер тимчасово недоступний. Спробуйте пізніше.");
+
+      if (status === 401) {
+        toast.error("Вам потрібно увійти до системи.");
+      } else if (status === 500 || status === 503) {
+        toast.error("Сервер не відповідає. Спробуйте пізніше.");
       }
     } else if (error.request) {
       console.log("Сервер не відповів:", error.request);
       toast.error("Сервер не відповідає. Спробуйте пізніше.");
     } else {
-      console.log("Помилка при налаштуванні запиту:", error.message);
+      console.log("Щось пішло не так при запиті:", error.message);
     }
   } else {
     console.error("Non-Axios error:", error);
