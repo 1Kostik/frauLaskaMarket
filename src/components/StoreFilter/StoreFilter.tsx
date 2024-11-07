64ecca609b5e5cc45c3d17e9deb4df67ac6de65c
1Kostik <sorochankostya@gmail.com>
2024-11-07 11:02:23 +0200

    fix bug

--- src/components/StoreFilter/StoreFilter.tsx
+++ src/components/StoreFilter/StoreFilter.tsx
@@ -52,8 +52,7 @@ const StoreFilter: React.FC<ISorteFilter> = ({
   const dispatch = useAppDispatch();
   const location = useLocation();
   const navigate = useNavigate();
-console.log('getSavedOpenCategories() :>> ', getSavedOpenCategories());
-console.log(' getSavedCheckedItems() :>> ',  getSavedCheckedItems());
+
   const [openCategories, setOpenCategories] = useState<{
     [key: number]: boolean;
   }>(getSavedOpenCategories() || {});
