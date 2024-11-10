8e10e6d1eabbd13ae177fbe567efb27dc5d9587a
1Kostik <sorochankostya@gmail.com>
2024-11-10 20:36:43 +0200

    up

--- src/components/Footer/Footer.styled.ts
+++ src/components/Footer/Footer.styled.ts
@@ -188,7 +188,7 @@ export const legalInfoContainer = css`
   ${onDesktop(css`
     display: flex;
     flex-direction: column;    
-    height: 70px;
+    height: 106px;
     width: 100%;
   `)}
 `;
